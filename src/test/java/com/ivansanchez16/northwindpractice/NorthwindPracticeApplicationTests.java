39568bbe924885ed86fdad9bad705769b8f3bc24
Ivan Sanchez Aispuro <ivan.sanchez1609@gmail.com>
2023-11-20 17:12:41 -0700

    Primera version del proyecto

---
+++ src/test/java/com/ivansanchez16/northwindpractice/NorthwindPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivansanchez16.northwindpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
